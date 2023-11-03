1ecafd96fa09af9f34aba040ba9dad2ce9785a76
AlexandreRCS <ale.maresias@gmail.com>
2023-11-03 02:10:21 -0300

    JS
    melhorando funcionalidades

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector('#btnTry')
+const btnTryAgain = document.querySelector('#btnTryAgain')
+const  screen1 = document.querySelector('.screen1')
+const  screen2 = document.querySelector('.screen2')
+let randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 
-function handleClick(event){
+function clickBtnTry(event){
     event.preventDefault()
     const inputNumber = document.querySelector("#inputNumber")
     if(Number(inputNumber.value) == randomNumber){
-        document.querySelector('.screen1').classList.add('hide')
-        document.querySelector('.screen2').classList.remove('hide')
-
+        handleClick()
         document.querySelector('.screen2 h2').innerHTML = `Acertou em ${attempts} Tentativas!`
     }
+    inputNumber.value = ""
     attempts ++
 }
+
+//Event
+
+btnTry.addEventListener('click', clickBtnTry)
+
+btnTryAgain.addEventListener('click', clickBtnTryAgain)
+
+function clickBtnTryAgain(){
+    handleClick()
+    randomNumber = Math.round(Math.random() * 10)
+    attempts = 1
+}
+
+function handleClick(){
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
+
+document.addEventListener('keydown', function(event){
+    console.log(event.key)
+    if(event.key == 'Enter' && screen1.classList.contains('hide')){
+        clickBtnTryAgain()
+    }
+} )
+
